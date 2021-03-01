57147beadcf288aefdde85e7cee44c43d9ea2f98
Si HaMaDa <nrookman_27@yahoo.com>
2021-03-02 01:52:11 +0200

    install audioPlayer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,15 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              child: Text('Let\'s sound'),
+              onPressed: () {
+                final audioCache = AudioCache();
+                audioCache.play('note7.wav');
+              },
+            ),
+          ),
         ),
       ),
     );
