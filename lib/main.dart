1a41bb5842b8f678955b52b02c9bad93bf44b62e
Si HaMaDa <nrookman_27@yahoo.com>
2021-03-02 02:11:31 +0200

    Build The App

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int num) {
+    final audioCache = AudioCache();
+    audioCache.play('note$num.wav');
+  }
+
+  Expanded buildKey({Color color, int num}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        child: Text('Let\'s sound'),
+        onPressed: () {
+          playSound(num);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              child: Text('Let\'s sound'),
-              onPressed: () {
-                final audioCache = AudioCache();
-                audioCache.play('note7.wav');
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, num: 1),
+              buildKey(color: Colors.orange, num: 2),
+              buildKey(color: Colors.yellow, num: 3),
+              buildKey(color: Colors.green, num: 4),
+              buildKey(color: Colors.teal, num: 5),
+              buildKey(color: Colors.blue, num: 6),
+              buildKey(color: Colors.purple, num: 7),
+            ],
           ),
         ),
       ),
